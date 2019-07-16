8cbbcb35aec5286e1bb0da2b4f6b51e3c3d87190
Bryan McClain <bamcclain10@gmail.com>
2019-07-15 21:09:48 -0400

    Wrote constants.h and variables.h files

---
+++ include/constants.h
@@ -0,0 +1,5 @@
+  SEG.U Constants
+
+; All constants used by the program
+LEFT_DOOR_X = 4
+RIGHT_DOOR_X = 160 - 10
---
+++ include/variables.h
@@ -0,0 +1,9 @@
+  SEG.U Variables
+  ORG $0080
+
+;All variables for the program
+playerX             ds 1
+playerY             ds 1
+
+backgroundColor     ds 1
+wallColor           ds 1
